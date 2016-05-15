55418bde0fb54422211fdd45f5e6e25528dc040d
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2016-05-15 11:48:57 -0300

    Fix virtualenv list

--- functions/__virtualenvs.fish
+++ functions/__virtualenvs.fish
@@ -1,3 +1,3 @@
 function __virtualenvs -a venv -d "list all virtualenvs"
-    ls -1 $PYENV_ROOT/versions/*/envs/ | grep "^$venv"
+    ls -1 -d $PYENV_ROOT/versions/*/envs/* | rev | cut -d '/' -f 1 | rev | grep "^$venv"
 end
