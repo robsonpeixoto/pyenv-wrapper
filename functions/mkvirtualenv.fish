dbeb6f7dbce0f598f0cd7f318c29912a4ebbc6ca
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2016-08-27 18:33:41 -0300

    Deactivate virtualenv before create another

--- functions/mkvirtualenv.fish
+++ functions/mkvirtualenv.fish
@@ -1,7 +1,12 @@
 function mkvirtualenv -a py_version venv_name -d "Create a virtualenv"
+    if test -n "$VIRTUAL_ENV"
+    	deactivate
+    end
+
     if test -z $py_version -o -z $venv_name
         echo "usage: mkvirtualenv <version> <virtualenv name>"
         exit 1
     end
     pyenv virtualenv $py_version $venv_name
+    workon $venv_name
 end
