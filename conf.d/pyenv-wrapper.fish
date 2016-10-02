48048ff0d25064390e9ee9a9e23a53b00c0f88f8
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2016-10-02 20:59:07 -0300

    Remove envset

--- conf.d/pyenv-wrapper.fish
+++ conf.d/pyenv-wrapper.fish
@@ -7,9 +7,3 @@ end
 if not echo (pyenv commands) | grep virtualenv  > /dev/null 2> /dev/null
     echo "Please, install pyenv-virtualenv: brew install pyenv-virtualenv"
 end
-
-set -l os (uname -s)
-switch $os
-    case Darwin
-        set -gx PYTHON_CONFIGURE_OPTS "--enable-unicode=ucs2"
-end
